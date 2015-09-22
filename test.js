6c9f84c6b776aab290a63fe9fdae03dd858d9855
SamathaU <Samatha.Uppula@mindtree.com>
2015-09-22 15:55:36 +0530

    Update test.js

--- test.js
+++ test.js
@@ -20,7 +20,7 @@ describe('User visits Hello World page', function() {
     });
 
     it('should see Hello World', function() {
-      browser.assert.text('h2', 'Hello World! mocho2');
+      browser.assert.text('h2', 'Hello World! paertrail');
     });
   });
 });
